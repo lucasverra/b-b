9af4010a4017bd5951b702f356ba3aadc00eefde
guram sanikidze <sanikidze.guram@gmail.com>
2019-04-29 00:29:53 +0400

    [up] validations

--- src/view/pages/Upload/validate.js
+++ src/view/pages/Upload/validate.js
@@ -2,15 +2,31 @@ import _ from 'underscore';
 
 export default (data) => {
     const errors = {};
+    const marque = data[0]['MARQUE / BRAND'];
+    const isAllLinkPhotoUnique = Object.keys(_.countBy(data, 'LINK PHOTO')).length === data.length;
+    const isAllReferencesBBUnique = Object.keys(_.countBy(data, 'REFERENCES  B&B')).length === data.length;
+    const isAllEanUnique = Object.keys(_.countBy(data, 'EAN')).length === data.length;
+
+    // unique records
+    if (!isAllEanUnique){
+        errors.nonUniqueEan = 'EAN must be unique';
+    }
+    if (!isAllLinkPhotoUnique){
+        errors.nonUniqueLink = 'LINK PHOTO must be unique';
+    }
+    if (!isAllReferencesBBUnique){
+        errors.nonUniqueReferences = 'REFERENCES  B&B must be unique';
+    }
 
     _.forEach(data, (item) => {
+        // required
         if (!item['LINK PHOTO']) {
             errors.linkPhoto = 'LINK PHOTO required';
         }
         if (!item['MARQUE / BRAND']) {
             errors.marqueBrand = 'Marque / Brand Required'
         }
-        if (!item['REFERENCES B&B']) {
+        if (!item['REFERENCES  B&B']) {
             errors.referencesBB = 'REFERENCES B&B Required';
         }
         if (!item['EAN']) {
@@ -27,6 +43,10 @@ export default (data) => {
         }
         if (!item['TYPE/ CATEGORIE PRODUIT']) {
             errors.type = 'TYPE/ CATEGORIE PRODUIT required';
+        }
+        // other
+        if (item['MARQUE / BRAND'] && item['MARQUE / BRAND'] !== marque) {
+            errors.brandTheSame = 'Brand must be the same';
         }
     });
 
