75b9b8c745636be8dc68d9e93f5b0cd788babc8a
guram sanikidze <sanikidze.guram@gmail.com>
2019-04-28 19:25:05 +0400

    [up] file upload functionality - display next button

--- src/view/pages/Upload/Upload.js
+++ src/view/pages/Upload/Upload.js
@@ -1,40 +1,54 @@
 import React from 'react';
-import {Upload as AntUpload, Icon} from 'antd';
-import { read, utils } from 'xlsx';
+import {Upload as AntUpload, Icon, Button} from 'antd';
+import {read, utils} from 'xlsx';
 
 const reader = new FileReader();
-reader.onload = (evt) => {
-    /* Parse data */
-    const bstr = evt.target.result;
-    const wb = read(bstr, {type: 'binary'});
-    /* Get first worksheet */
-    const wsname = wb.SheetNames[0];
-    const ws = wb.Sheets[wsname];
-    /* Convert array of arrays */
-    const data = utils.sheet_to_json(ws, {header: 1});
-    /* Update state */
-    console.log(data);
-};
-
-const draggerConfig = {
-    name: 'file',
-    multiple: false,
-    accept: '.xlsx',
-    action: (file) => {
-        reader.readAsBinaryString(file);
-    }
-};
 
 class Upload extends React.Component {
+    state = {
+        fileData: null,
+    };
+
+    componentDidMount() {
+        reader.onload = (evt) => {
+            /* Parse data */
+            const bstr = evt.target.result;
+            const wb = read(bstr, {type: 'binary'});
+            /* Get first worksheet */
+            const wsname = wb.SheetNames[0];
+            const ws = wb.Sheets[wsname];
+            /* Convert array of arrays */
+            const data = utils.sheet_to_json(ws, {header: 1});
+            /* Update state */
+            this.setState({
+                fileData: data,
+            });
+        };
+    }
+
+    draggerConfig = {
+        name: 'file',
+        multiple: false,
+        accept: '.xlsx',
+        action: (file) => {
+            reader.readAsBinaryString(file);
+        }
+    };
+
     render() {
+        const { fileData } = this.state;
+
         return (
             <div>
-                <AntUpload.Dragger {...draggerConfig}>
+                <AntUpload.Dragger {...this.draggerConfig}>
                     <p className="ant-upload-drag-icon">
                         <Icon type="inbox"/>
                     </p>
                     <p className="ant-upload-text">Click or drag file to this area to upload</p>
                 </AntUpload.Dragger>
+                {
+                    fileData && <Button type="primary">Next</Button>
+                }
             </div>
         )
     }
