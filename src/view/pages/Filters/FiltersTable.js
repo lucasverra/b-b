6aab2e07345fa73eaaf563e05da01f2b0b7d2b6f
guram sanikidze <sanikidze.guram@gmail.com>
2019-05-01 09:23:37 +0400

    [new] table filters

--- src/view/pages/Filters/FiltersTable.js
+++ src/view/pages/Filters/FiltersTable.js
@@ -1,52 +1,73 @@
 import React from 'react';
-import { Table } from 'antd'
+import { Table } from 'antd';
+import _ from 'underscore';
 
-const columns = [
-    {
-        dataIndex: 'NO.',
-        title: '#',
-        width: '5%',
-    },
-    {
-        dataIndex: 'REFERENCES  B&B',
-        title: 'CODE B&B',
-        sorter: (a, b) => a > b ? -1 : 1,
-        width: '20%',
-    },
-    {
-        dataIndex: 'NOM  MODELE / MODEL NAME',
-        title: 'NOM',
-        sorter: (a, b) => a > b ? -1 : 1,
-        width: '20%',
-    },
-    {
-        dataIndex: 'TYPE/ CATEGORIE PRODUIT',
-        title: 'CATEGORIE',
-        sorter: (a, b) => a > b ? -1 : 1,
-        width: '35%',
-    },
-    {
-        dataIndex: 'COULEUR',
-        title: 'COULEUR',
-        sorter: (a, b) => a > b ? -1 : 1,
-        width: '10%',
-    },
-    {
-        dataIndex: 'COULEUR PIEDS',
-        title: 'PIEDS',
-        sorter: (a, b) => a > b ? -1 : 1,
-        width: '10%',
-    },
-];
+const dataIndexes = ['NO.', 'REFERENCES  B&B', 'NOM  MODELE / MODEL NAME', 'TYPE/ CATEGORIE PRODUIT', 'COULEUR', 'COULEUR PIEDS'];
+const titles = ['#', 'CODE B&B', 'NOM', 'CATEGORIE', 'COULEUR', 'PIEDS'];
+const sorter = (a, b) => a > b ? -1 : 1;
+const filter = (val, record, col) => record[col] === val;
 
-const FiltersTable = ({ dataSource }) => (
-    <Table
-        dataSource={dataSource}
-        columns={columns}
-        pagination={{
-            showSizeChanger: true
-        }}
-    />
-);
+const FiltersTable = ({ dataSource }) => {
+    const filtersUniqueVals = {};
+    _.forEach(dataIndexes.slice(1), (col, i) => {
+        filtersUniqueVals[col] = _.countBy(dataSource, col);
+    });
+
+    const columns = [
+        {
+            dataIndex: dataIndexes[0],
+            title: titles[0],
+            width: '5%',
+        },
+        {
+            dataIndex: dataIndexes[1],
+            title: titles[1],
+            sorter,
+            width: '20%',
+        },
+        {
+            dataIndex: dataIndexes[2],
+            title: titles[2],
+            sorter,
+            width: '20%',
+            filters: _.map(Object.keys(filtersUniqueVals[dataIndexes[2]]), (key) => ({ value: key, text: key })),
+            onFilter: (val, record) => filter(val, record, dataIndexes[2]),
+        },
+        {
+            dataIndex: dataIndexes[3],
+            title: titles[3],
+            sorter,
+            width: '35%',
+            filters: _.map(Object.keys(filtersUniqueVals[dataIndexes[3]]), (key) => ({ value: key, text: key })),
+            onFilter: (val, record) => filter(val, record, dataIndexes[3]),
+        },
+        {
+            dataIndex: dataIndexes[4],
+            title: titles[4],
+            sorter,
+            width: '10%',
+            filters: _.map(Object.keys(filtersUniqueVals[dataIndexes[4]]), (key) => ({ value: key, text: key })),
+            onFilter: (val, record) => filter(val, record, dataIndexes[4]),
+        },
+        {
+            dataIndex: dataIndexes[5],
+            title: titles[5],
+            sorter,
+            width: '10%',
+            filters: _.map(Object.keys(filtersUniqueVals[dataIndexes[5]]), (key) => ({ value: key, text: key })),
+            onFilter: (val, record) => filter(val, record, dataIndexes[5]),
+        },
+    ];
+
+    return (
+        <Table
+            dataSource={dataSource}
+            columns={columns}
+            pagination={{
+                showSizeChanger: true
+            }}
+        />
+    )
+};
 
 export default FiltersTable
