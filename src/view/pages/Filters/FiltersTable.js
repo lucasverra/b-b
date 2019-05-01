c7b07c29bae2614b1c2b52fdeb8a7b294627becf
guram sanikidze <sanikidze.guram@gmail.com>
2019-05-01 08:54:31 +0400

    [new] table sorters

--- src/view/pages/Filters/FiltersTable.js
+++ src/view/pages/Filters/FiltersTable.js
@@ -5,26 +5,37 @@ const columns = [
     {
         dataIndex: 'NO.',
         title: '#',
+        width: '5%',
     },
     {
         dataIndex: 'REFERENCES  B&B',
         title: 'CODE B&B',
+        sorter: (a, b) => a > b ? -1 : 1,
+        width: '20%',
     },
     {
         dataIndex: 'NOM  MODELE / MODEL NAME',
         title: 'NOM',
+        sorter: (a, b) => a > b ? -1 : 1,
+        width: '20%',
     },
     {
         dataIndex: 'TYPE/ CATEGORIE PRODUIT',
         title: 'CATEGORIE',
+        sorter: (a, b) => a > b ? -1 : 1,
+        width: '35%',
     },
     {
         dataIndex: 'COULEUR',
         title: 'COULEUR',
+        sorter: (a, b) => a > b ? -1 : 1,
+        width: '10%',
     },
     {
         dataIndex: 'COULEUR PIEDS',
         title: 'PIEDS',
+        sorter: (a, b) => a > b ? -1 : 1,
+        width: '10%',
     },
 ];
 
